621129191d58e1e6c3f463f706ab5378c554383d
Domniki Barbari <dombarbari@gmail.com>
2026-01-29 11:01:02 +0200

    Trigger detector added, pending testing

---
+++ groupProject/Assets/Resources/GrabTestScene/Scripts/CollisionDetector.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Level0"))
+        {
+            Debug.Log("Level0 collision happened with object " + other.gameObject.name);
+        }
+        else if (other.gameObject.CompareTag("Level1"))
+        {
+            Debug.Log("Level1 collision happened with object " + other.gameObject.name);
+        }
+        else if (other.gameObject.CompareTag("Level2"))
+        {
+            Debug.Log("Level2 collision happened with object " + other.gameObject.name);
+        }
+    }
+
+}
