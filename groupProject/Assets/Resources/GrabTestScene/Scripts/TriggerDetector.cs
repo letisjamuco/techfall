7c86c8cdd267e8e84039a9a7afa6aa27a76879ec
Domniki Barbari <dombarbari@gmail.com>
2026-01-30 13:04:27 +0200

    Tagged objects collide with levels' isTrigger boxes

--- groupProject/Assets/Resources/GrabTestScene/Scripts/TriggerDetector.cs
+++ groupProject/Assets/Resources/GrabTestScene/Scripts/TriggerDetector.cs
@@ -4,31 +4,14 @@ using UnityEngine;
 
 public class TriggerDetector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //Tag to be compared
+    [SerializeField] string tagField;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Level0"))
+        if (other.gameObject.CompareTag(tagField))
         {
-            Debug.Log("Level0 collision happened with object " + other.gameObject.name);
-        }
-        else if (other.gameObject.CompareTag("Level1"))
-        {
-            Debug.Log("Level1 collision happened with object " + other.gameObject.name);
-        }
-        else if (other.gameObject.CompareTag("Level2"))
-        {
-            Debug.Log("Level2 collision happened with object " + other.gameObject.name);
+            Debug.Log(tagField + " collision happened");
         }
     }
 
